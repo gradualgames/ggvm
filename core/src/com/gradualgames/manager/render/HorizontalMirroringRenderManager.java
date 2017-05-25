c4de23eb5852a566d26660a1310243694925a90f
Derek Andrews <gradualgames@gmail.com>
2017-05-25 17:16:39 -0400

    Handle scroll Y being greater than 239 and less than or equal to 255. This allows vertical screen shaking effects to work properly.

--- core/src/com/gradualgames/manager/render/HorizontalMirroringRenderManager.java
+++ core/src/com/gradualgames/manager/render/HorizontalMirroringRenderManager.java
@@ -1,5 +1,6 @@
 package com.gradualgames.manager.render;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.gradualgames.ggvm.BusListener;
@@ -48,6 +49,12 @@ public class HorizontalMirroringRenderManager extends RenderManager implements B
     private void drawNametable(GGVm ggvm, SpriteBatch spriteBatch, int scrollX, int scrollY, float splitYStart, float splitYEnd) {
         int patternTableOffset = ggvm.getBackgroundPatternTableAddress() == 0 ? 0 : 1;
 
+        boolean toggleNametableEarly = false;
+        if (scrollY > 239 && scrollY <= 255) {
+            scrollY -= 16;
+            toggleNametableEarly = true;
+        }
+
         int coarseScrollX = scrollX >> 3;
         int coarseScrollY = scrollY >> 3;
         int fineScrollX = scrollX & 7;
@@ -68,6 +75,9 @@ public class HorizontalMirroringRenderManager extends RenderManager implements B
             nameTableRowCount = fineScrollY == 0 ? 30 : 31;
             nameTableY = coarseScrollY;
             nameTable = (ggvm.getNametableAddress() == Ppu.NAME_TABLE_0_BASE_ADDRESS) ? 0 : 1;
+            if (toggleNametableEarly) {
+                nameTable ^= 1;
+            }
             while (nameTableRowCount > 0) {
                 actualNameTableX = nameTableX % 32;
                 actualNameTableY = nameTableY % 30;
@@ -83,7 +93,7 @@ public class HorizontalMirroringRenderManager extends RenderManager implements B
                 int indexColumn = index & 0x0f;
                 Sprite sprite = patternTableSprites[patternTableOffset * 16 + indexRow][indexColumn];
                 sprite.setColor(0, attributes[attribute], 0, 0);
-                if (screenY >= splitYStart && screenY < splitYEnd) {
+                if (!(sprite0HitStatusBarEnabled && sprite0HitStatusBarOn) || (screenY >= splitYStart && screenY < splitYEnd)) {
                     sprite.setPosition(screenX - fineScrollX, 232 - screenY + fineScrollY);
                     sprite.draw(spriteBatch);
                 }
